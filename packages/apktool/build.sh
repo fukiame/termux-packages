5dfee98e2950fe2039a7206eb8cd9b91b4a7d8a5
fukiame <fukiame@proton.me>
2025-07-23 08:58:06 +0700

    bump(main/apktool): 2.12.0
    Signed-off-by: fukiame <fukiame@proton.me>

--- packages/apktool/build.sh
+++ packages/apktool/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://ibotpeaches.github.io/Apktool/
 TERMUX_PKG_DESCRIPTION="A tool for reverse engineering 3rd party, closed, binary Android apps"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.1
+TERMUX_PKG_VERSION=2.12.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/iBotPeaches/Apktool/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8932e77d963b9e0e07227422d82ed4a355e8aa268bad1361e5cfaffa8e4d52ee
+TERMUX_PKG_SHA256=d87a589778d4369362a99c83a67ca3d365891395aba1be1b85dbf04e72cd3298
 TERMUX_PKG_DEPENDS="aapt, aapt2, openjdk-17"
 TERMUX_PKG_BUILD_IN_SRC=true
 
